30a7f5c92e2211f75a8b26fc3c326955e2509514
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-11-26 13:13:45 +0900

    Main.elm を変更して、app.js を更新

--- touch-event.js
+++ touch-event.js
@@ -13,6 +13,7 @@
         // Touchstartリスナー: タッチポイント数を送信
         document.addEventListener("touchstart", (event) => {
             const touchCount = event.touches.length; // 同時にタッチしている指の数
+            console.log(`Touchstart detected with ${touchCount} fingers.`);
             if (app.ports && app.ports.touchStart) {
                 app.ports.touchStart.send(touchCount);
             }
@@ -20,6 +21,7 @@
 
         // Touchendリスナー: タッチ終了イベント
         document.addEventListener("touchend", () => {
+            console.log("Touchend detected.");
             if (app.ports && app.ports.touchEnd) {
                 app.ports.touchEnd.send(null);
             }
