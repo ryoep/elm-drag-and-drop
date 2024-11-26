dbc27dc86c9de36a421c6a9c3794e46a62230f3a
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-11-26 13:05:38 +0900

    Main.elm を変更して、app.js を更新

--- src/Main.elm
+++ src/Main.elm
@@ -50,7 +50,7 @@ view model =
     div
         [ style "height" "100vh"
         , style "width" "100vw"
-        , style "background-color" "lightgreen"
+        , style "background-color" "lightblue"
         ]
         [ text model.message ]
 
