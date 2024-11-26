39b761d2181556c79ea23a17c53b77b4412c7510
ryoep <ep21103-9124@sti.chubu.ac.jp>
2024-11-26 09:27:36 +0900

    Main.elm を変更して、app.js を更新

--- src/Main.elm
+++ src/Main.elm
@@ -6,18 +6,22 @@ import Html.Attributes exposing (style)
 import Html.Events exposing (on)
 import Json.Decode as Decode
 
+
 -- MODEL
 type alias Model =
     { message : String }
+
 
 initialModel : Model
 initialModel =
     { message = "タッチを試してください" }
 
+
 -- MESSAGE
 type Msg
     = TouchStart Int -- タッチポイント数を受け取る
     | TouchEnd
+
 
 -- UPDATE
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -41,24 +45,27 @@ update msg model =
         TouchEnd ->
             ( { model | message = "Touchend detected!" }, Cmd.none )
 
+
 -- VIEW
 view : Model -> Html Msg
 view model =
     div
         [ style "height" "100vh"
         , style "width" "100vw"
-        , style "background-color" "lightgreen"
-        , style "touch-action" "none" -- デフォルト動作を無効化
+        , style "background-color" "green"
+        , style "touch-action" "none"
         , on "touchstart" (Decode.map TouchStart touchCountDecoder)
         , on "touchend" (Decode.succeed TouchEnd)
         ]
         [ text model.message ]
+
 
 -- DECODER
 touchCountDecoder : Decode.Decoder Int
 touchCountDecoder =
     Decode.field "touches" (Decode.list Decode.value)
         |> Decode.map List.length
+
 
 -- MAIN
 main : Program () Model Msg
